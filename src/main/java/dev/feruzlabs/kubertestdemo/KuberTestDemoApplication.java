1bee71b924ad9bb3f5d17f0bc2562a76479e867b
Feruz <feruzostonov3@gmail.com>
2026-02-22 21:58:35 +0500

    first commit

---
+++ src/main/java/dev/feruzlabs/kubertestdemo/KuberTestDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.feruzlabs.kubertestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberTestDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuberTestDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/feruzlabs/kubertestdemo/KuberTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.feruzlabs.kubertestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberTestDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
